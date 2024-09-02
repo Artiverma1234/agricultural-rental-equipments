7558cc4ca3a3b91672c112efd282cb583cdbadfb
Artiverma1234 <sssmohitv033@gmail.com>
2024-09-02 01:10:19 -0700

    first commit

---
+++ frontend/src/equipmentData.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+        image: 'https://pre-live-admin.balwaan.com/uploads/media/2024/BP-650-white-background.webp',
+        title: 'HAND WEEDER',
+        category: 'WEEDER',
+        price: 379,
+        brand: 'TAPAS'
+    },
+    {
+        image: 'https://pre-live-admin.balwaan.com/uploads/media/2023/13.jpg',
+        title: 'BS-20 Single Motor',
+        category: 'Motor Battery Sprayer',
+        price: 3440,
+        brand: 'BALWAAN'
+    },
+    {
+        image: 'https://pre-live-admin.balwaan.com/uploads/media/2024/1_(10)1.jpg',
+        title: 'WATER PUMPS',
+        category:'Water Pumps',
+        price: 12600,
+        brand: 'BALWAAN'
+    },
+    {
+        image: 'https://agribegri.com/admin/images/prod_image/thumb/thumb_67325be04870cc34e0cdd2af1c38f866-07-07-19-22-12-12.webp',
+        title: ' FLOWER SEED ',
+        category:'seed',
+        price: 204,
+        brand: 'RK Seeds '
+    },
+    {
+        image: 'https://agribegri.com/admin/images/prod_image/thumb/thumb_8b210c952fd73db07f615e7cabe95093-04-20-21-11-17-04.webp',
+        title: 'GREEN GARDEN SHADE NET  ',
+        Categery:'garden Net',
+        price: 2030,
+        brand: 'RK Seeds'
+    },
+
+]
